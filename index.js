e5648cbec2f276de76cb0b2a56ae2bd9b293df75
XKostikiadis <xristoskostikiadis@gmail.com>
2022-06-21 21:19:30 +0300

    KryptoBux: Team Folders

--- index.js
+++ index.js
@@ -3,10 +3,10 @@ const { startCreating, buildSetup } = require(`${basePath}/src/main.js`);
 const { startSorting, startRenaming } = require(`${basePath}/src/script.js`);
 
 (() => {
-  // startSorting();
+  startSorting();
   // startRenaming();
-  for (let index = 0; index < 3; index++) {
-    buildSetup(index);
-    startCreating(index);
-  }
+  // for (let index = 0; index < 3; index++) {
+  //   buildSetup(index);
+  //   startCreating(index);
+  // }
 })();
